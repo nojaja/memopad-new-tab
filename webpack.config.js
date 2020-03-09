6433d4ea9cd5b079043c043b05a2f586556ec398
nojaja <free.riccia@gmail.com>
2020-03-09 19:03:58 +0900

    distからcssとjson、htmlを分離

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
 const path = require('path');
+const src = __dirname + "/src";
+const dist = __dirname + "/dist";
 const webpack = require('webpack');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyFilePlugin = require("copy-webpack-plugin");
+const WriteFilePlugin = require("write-file-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  devtool: 'source-map',
+  context: src,
+  entry: {
+    main: './js/index.js',
+    style: './css/index.css'
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    path: dist,
   },
   module: {
     rules: [{
       test: /\.css$/,
-      use: ['style-loader', 'css-loader']
+      use: [{ // MiniCssExtractPlugin.loaderÇéwíË
+               loader: MiniCssExtractPlugin.loader,
+               options: {
+                    publicPath: '../',
+               },
+            },
+            'css-loader'
+      ]
+    }, {
+      test: /\.html$/,
+      use: ['html-loader']
     }, {
       test: /\.ttf$/,
       use: ['file-loader']
@@ -22,6 +43,23 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery'
     }),
-    new MonacoWebpackPlugin()
+    new MonacoWebpackPlugin(),
+    new HtmlWebpackPlugin({//htmlÇsrcîzâ∫Ç≈ä«óùâ¬î\Ç…Ç∑ÇÈ
+      template: "./html/index.html"
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'css/[name].css',
+    }),
+    new CopyFilePlugin(
+        [
+            {
+                context: "src",
+                from: "assets/*.json",
+                to: dist
+            }
+        ],
+        { copyUnmodified: true }
+    ),
+    new WriteFilePlugin()
   ]
 };
