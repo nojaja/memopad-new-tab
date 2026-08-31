75814bada7f689c30e288d137dc70b20ae0d3a7d
nojaja <free.riccia@gmail.com>
2026-08-31 11:59:21 +0900

    fix: [AIDD] Monaco Workerのimportパスを修正
    - Monaco Editorの公開エントリポイントに合わせてWorker参照を更新
    - Viteビルド時のWorkerモジュール解決を安定させる

--- src/js/main.ts
+++ src/js/main.ts
@@ -4,11 +4,11 @@ import store from '@/store'
 import lang from '@/lang'
 import UniconCompat from '@/components/UniconCompat.vue'
 import './Debug'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker'
-import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker'
-import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker'
-import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker'
-import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker'
+import editorWorker from 'monaco-editor/editor/editor.worker?worker'
+import jsonWorker from 'monaco-editor/language/json/json.worker?worker'
+import cssWorker from 'monaco-editor/language/css/css.worker?worker'
+import htmlWorker from 'monaco-editor/language/html/html.worker?worker'
+import tsWorker from 'monaco-editor/language/typescript/ts.worker?worker'
 
 const monacoEnvironmentTarget = self as unknown as {
 	MonacoEnvironment?: {
